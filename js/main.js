5117d3b67575998e79e76e0d7ca03cd4148beedf
Gabriella G. R <g.rozenaite@gmail.com>
2018-06-06 21:35:22 +0200

    leaves cover added

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ const pageHome = document.getElementById('page__home');
 const pageAbout = document.getElementById('page__about');
 const pagePortfolio = document.getElementById('page__portfolio');
 const pageContact = document.getElementById('page__contact');
+const welcomeText = document.getElementById('text__greeting');
+
 
 // buttons
 const imgAbout = document.getElementById('img__page--about');
+const imgPortfolio = document.getElementById('img__page--portfolio');
+const imgContact = document.getElementById('img__page--contact');
+const btnExit = document.querySelector('.btn__exit');
 
 // menu
 
@@ -14,14 +19,48 @@ const imgAbout = document.getElementById('img__page--about');
 // FUNCTIONS
 // general
 
-// page home
+//menu
+btnExit.addEventListener('click', function(){
+    pageHome.style.display = 'block';
+    pageAbout.style.display = "none";
+    pagePortfolio.style.display = "none";
+})
 
-// page about
-function pageOpen(){
-
-}
+// page home - language changer
+let counter = 0;
+setInterval(function(){
+  if (counter == 0) {
+    counter ++;
+    welcomeText.innerHTML = '< BIENVENIDO />';
+  }
+  else if (counter == 1) {
+    welcomeText.innerHTML = '< VELCOMMEN />';
+    counter ++;
+  }
+  else if (counter == 2) {
+    welcomeText.innerHTML = '< SVEIKI />';
+    counter ++;
+  }
+  else if (counter == 3) {
+    welcomeText.innerHTML = '< ΚΑΛΩΣ ΗΡΘΑΤΕ />';
+    counter ++;
+  }
+  else {
+      welcomeText.innerHTML = '< WELCOME />';
+      counter = 0;
+  }
+}, 2800);
+// images directing to other pages
 imgAbout.addEventListener('click', function() {
-  console.log('hi');
   pageHome.style.display = 'none';
-  pageAbout.style.display = 'initial';
+  pageAbout.style.display = 'grid';
 });
+imgPortfolio.addEventListener('click', function() {
+  pageHome.style.display = 'none';
+  pagePortfolio.style.display = 'grid';
+});
+imgContact.addEventListener('click', function() {
+  pageHome.style.display = 'none';
+  pageContact.style.display = 'grid';
+});
+// page about
