c436032e13ee362a92ecca3182c5b8a198635968
Gabriella G. R <g.rozenaite@gmail.com>
2018-06-07 12:04:15 +0200

    Changed home page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 // VARIABLES
-// pages
+
+//cover leaves
+const leafTopRight = document.querySelector('.plant__top--right');
+const leafBottomLeft = document.querySelector('.plant__bottom--left');
+const leafBottomRight = document.querySelector('.plant__bottom--right');
+const leafTopLeft = document.querySelector('.plant__top--left');
+const leafTopMiddle = document.querySelector('.plant__top--middle');
+const leafLeftMiddle = document.querySelector('.plant__left--middle');
+
+
+//pages
 const pageHome = document.getElementById('page__home');
 const pageAbout = document.getElementById('page__about');
 const pagePortfolio = document.getElementById('page__portfolio');
@@ -7,17 +17,18 @@ const pageContact = document.getElementById('page__contact');
 const welcomeText = document.getElementById('text__greeting');
 
 
-// buttons
+//buttons
 const imgAbout = document.getElementById('img__page--about');
 const imgPortfolio = document.getElementById('img__page--portfolio');
 const imgContact = document.getElementById('img__page--contact');
 const btnExit = document.querySelector('.btn__exit');
 
-// menu
+//menu
 
 
 // FUNCTIONS
-// general
+
+//general
 
 //menu
 btnExit.addEventListener('click', function(){
@@ -26,7 +37,19 @@ btnExit.addEventListener('click', function(){
     pagePortfolio.style.display = "none";
 })
 
-// page home - language changer
+//page LEAVES COVER
+window.addEventListener('load', function(){
+  setTimeout(function(){
+    leafBottomRight.classList.add('anim_leafBR');
+    leafBottomLeft.classList.add('anim_leafBL');
+    leafTopRight.classList.add('anim_leafTR');
+    leafTopLeft.classList.add('anim_leafTL');
+    leafTopMiddle.classList.add('anim_leafTM');
+    leafLeftMiddle.classList.add('anim_leafLM');
+  }, 1000);
+})
+
+//page HOME - language changer
 let counter = 0;
 setInterval(function(){
   if (counter == 0) {
@@ -50,6 +73,7 @@ setInterval(function(){
       counter = 0;
   }
 }, 2800);
+
 // images directing to other pages
 imgAbout.addEventListener('click', function() {
   pageHome.style.display = 'none';
@@ -63,4 +87,4 @@ imgContact.addEventListener('click', function() {
   pageHome.style.display = 'none';
   pageContact.style.display = 'grid';
 });
-// page about
+//page about
